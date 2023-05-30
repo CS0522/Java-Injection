ca58792be01c3e9f50f9eda05d91b261b408b141
Frank <812359326@qq.com>
2023-05-30 19:50:51 +0800

    23/05/30: ignore some files

--- 07_models/java-detection-python1.0/make.sh
+++
@@ -1,5 +0,0 @@
-cp conf/ognl* engine/detection/grammar/
-cd engine/detection && sh make.sh
-cd ../..
-mv engine/detection/detection.so engine/
-python3 script/update.py
---
+++ README.md
@@ -0,0 +1,5 @@
+# 基于语法分析的表达式注入攻击模型
+> 07_models/, docs/ 等文件加入 .gitignore
+
+## 学习笔记
+[表达式注入_Learning.md](./00_Learning/表达式注入_Learning.md)
